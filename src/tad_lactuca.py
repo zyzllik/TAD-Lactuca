98ea8bee9e26e60f0c2b44e30abe6d5caa4d6723
LoopGan <ganwei4955@gmail.com>
2017-12-05 17:39:43 +0800

    update the tad_lactuca.py

--- src/tad_lactuca.py
+++ src/tad_lactuca.py
@@ -14,16 +14,14 @@ from src.utils import get_signal_plot
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("If you want use your data, please run script as:  \n\t  python3 tad_lcatuca.py ['the path to the data']")
-        feature_data = "../cache/E017_new/feature/bin_10_400kb.xlsx"
+        feature_data = "../cache/E017/feature/bin_10_400kb.xlsx"
         # feature_data = "../cache/E017_new/feature/all.xlsx"
     else:
         feature_data = sys.argv[1]
-    # print("CNN...")
-    # cnn_model.cnn_result(feature_data)
     print("MLP...")
     mlp_model.mlp_result(feature_data)
-    # print("RF...")
-    # rf_model.rf_result(feature_data)
+    print("RF...")
+    rf_model.rf_result(feature_data)
     # print("Plot...")
     # get_signal_plot.plot_dis(feature_file=feature_data, label_file=get_signal_plot.label_file, cell_line='IMR90',
     #                          feature_dimensional=get_signal_plot.feature_dimensional)
